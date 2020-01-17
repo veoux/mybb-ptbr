b3a5c0f7da04f55a755497a6dd8ba528d19f49ed
veoux <veoux@hotmail.com>
2020-01-17 08:37:34 -0300

    Core Files

---
+++ Upload/inc/languages/portuguese_brazil/announcements.lang.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * MyBB 1.8 Portuguese (Brazil) Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ * Translated by veoux with references to the Portuguese (Portugal) language of begincaos
+ */
+ 
+$l['nav_announcements'] = "Anúncio do Fórum";
+$l['announcements'] = "Anúncio";
+$l['forum_announcement'] = "Anúncio do Fórum: {1}";
+$l['error_invalidannouncement'] = "O anúncio especificado é inválido.";
+
+$l['announcement_edit'] = "Editar este anúncio";
+$l['announcement_qdelete'] = "Apagar este anúncio";
+$l['announcement_quickdelete_confirm'] = "Você tem certeza que quer apagar este anúncio?";
+
---
+++ Upload/inc/languages/portuguese_brazil/archive.lang.php
@@ -0,0 +1,18 @@
+<?php
+/**
+ * MyBB 1.8 Portuguese (Brazil) Language Pack
+ * Copyright 2014 MyBB Group, All Rights Reserved
+ * Translated by veoux with references to the Portuguese (Portugal) language of begincaos
+ */
+ 
+$l['archive_fullversion'] = "Versão Completa:";
+$l['archive_replies'] = "Respostas";
+$l['archive_reply'] = "Resposta";
+$l['archive_pages'] = "Páginas:";
+$l['archive_note'] = "Você está vendo uma versão reduzida de nosso conteúdo. <a href=\"{1}\">Veja a versão completa</a> com a formatação adequada.";
+$l['archive_nopermission'] = "Desculpe, você não tem permissão para acessar este recurso.";
+$l['error_nothreads'] = "Atualmente não há tópicos neste fórum.";
+$l['error_nopermission'] = "Você não tem permissão para ver os tópicos deste fórum.";
+$l['error_unapproved_thread'] = "Este tópico não foi aprovado. Por favor, <a href=\"{1}\">veja a versão completa</a> para ver o conteúdo deste tópico.";
+$l['archive_not_found'] = "A página solicitada não foi encontrada neste servidor.";
+$l['error_mustlogin'] = "Este fórum exige que todos os usuários façam o login.";
